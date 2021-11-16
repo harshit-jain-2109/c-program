f10890c5d37f260efa6c90a51c073c7b9edf0ea7
harshit <harshitjain3349@gmail.com>
2021-11-17 01:37:11 +0530

    c++ program

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{  float a,r;
+
+   cout<<"enter radius of circle ";
+   cin>>r;
+   a=3.14*r*r;
+   cout<<"area of required circle is "<<" "<<a<<endl;
+   cout<<"thank you for visiting"<<endl;	
+	
+   return 0;
+}
